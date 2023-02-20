059455ef160465c0a5f71d1eb479800cd432fb22
linooj <linooj@ttu.ee>
2023-02-20 21:26:17 +0200

    *added createStaticBody and createPolygonShape methods

--- libGDX/core/src/helper/TileMapHelper.java
+++ libGDX/core/src/helper/TileMapHelper.java
@@ -8,11 +8,16 @@ import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.Shape;
 import com.pigeon.game.GameScreen;
 import objects.player.Player;
 
-import java.awt.*;
+
+import static helper.Constants.PPM;
 
 public class TileMapHelper {
 
@@ -23,16 +28,19 @@ public class TileMapHelper {
         this.gameScreen = gameScreen;
     }
 
-    //TODO setupMap function
-
     public OrthogonalTiledMapRenderer setUpMap() {
-        tiledMap = new TmxMapLoader().load("maps/map0.tmx");
+        tiledMap = new TmxMapLoader().load("../libGDX/assets/maps/maps0.tmx");
+        parseMapObjects(tiledMap.getLayers().get("objects").getObjects());
         return new OrthogonalTiledMapRenderer(tiledMap);
+
     }
 
     private void parseMapObjects(MapObjects mapObjects) {
         for(MapObject mapObject : mapObjects) {
 
+            if (mapObject instanceof PolygonMapObject) {
+                createStaticBody((PolygonMapObject) mapObject) ;
+            }
             if(mapObject instanceof RectangleMapObject) {
                 Rectangle rectangle = ((RectangleMapObject) mapObject).getRectangle();
                 String rectangleName = mapObject.getName();
@@ -50,5 +58,27 @@ public class TileMapHelper {
                 }
             }
         }
+    }
+
+    private void createStaticBody(PolygonMapObject polygonMapObject) {
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyDef.BodyType.StaticBody;
+        Body body = gameScreen.getWorld().createBody(bodyDef);
+        Shape shape = createPolygonShape(polygonMapObject);
+        body.createFixture(shape, 1000);
+        shape.dispose();
+    }
+
+    private Shape createPolygonShape(PolygonMapObject polygonMapObject) {
+        float[] vertices = polygonMapObject.getPolygon().getTransformedVertices();
+        Vector2[] worldVertices = new Vector2[vertices.length / 2];
+
+        for (int i=0; i< vertices.length/2; i++) {
+            Vector2 current = new Vector2(vertices[i*2]/PPM, vertices[i*2+1]/PPM);
+            worldVertices[i] = current;
+        }
+        PolygonShape shape = new PolygonShape();
+        shape.set(worldVertices);
+        return shape;
     }
 }
