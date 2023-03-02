7e1c3beb83fa601b0d438227ff1144674fcf6bc1
aleksandra.anikina <aleksandra_anikina@hotmail.com>
2023-03-02 16:00:37 +0200

    Renewed map and tiles, again

--- Client/assets/maps/GrassNGrass.tsx
+++ Client/assets/maps/GrassNGrass.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="GrassNGrass" tilewidth="16" tileheight="16" tilecount="56" columns="8">
- <image source="GrassNGrass.png" width="128" height="127"/>
+<tileset version="1.9" tiledversion="1.9.2" name="Grass" tilewidth="16" tileheight="16" tilecount="56" columns="8">
+ <image source="GrassNGrass.png" width="128" height="122"/>
 </tileset>
--- Client/assets/maps/pigeonsTiles.tsx
+++ Client/assets/maps/pigeonsTiles.tsx
@@ -1,14 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="pigeons" tilewidth="16" tileheight="16" tilecount="110" columns="11">
- <editorsettings>
-  <export target="pigeonsTiles.tsx" format="tsx"/>
- </editorsettings>
+<tileset version="1.9" tiledversion="1.9.2" name="pigeonsTiles" tilewidth="16" tileheight="16" tilecount="110" columns="11">
  <image source="pigeonsTiles.png" width="176" height="160"/>
  <tile id="13">
   <animation>
-   <frame tileid="11" duration="150"/>
-   <frame tileid="12" duration="150"/>
-   <frame tileid="13" duration="150"/>
+   <frame tileid="11" duration="125"/>
+   <frame tileid="12" duration="125"/>
+   <frame tileid="13" duration="125"/>
   </animation>
  </tile>
 </tileset>
